c992065e097d126ec9c9d641ba3fe21a4a476339
photomelabs <58054846+photomelabs@users.noreply.github.com>
2019-11-22 10:05:07 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'Demo contact form <photomelabs@gmail.com>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = 'Demo contact form <photomelabs@gmail.com>';
 
 // subject of the email
 $subject = 'New message from contact form';
